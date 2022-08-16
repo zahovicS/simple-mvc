640d7d33f3df6479ba752e1cf07bbef5d00d801d
zahovic Saravia Suarez <49574508+zahovicS@users.noreply.github.com>
2022-08-16 12:39:17 +0200

    include request

--- App/kernel.php
+++ App/kernel.php
@@ -4,6 +4,7 @@ require_once dirname(__DIR__) . "/App/Configs/Configs.php";
 require_once dirname(__DIR__) . "/App/Libs/Base.php";
 require_once dirname(__DIR__) . "/App/Libs/Controller.php";
 require_once dirname(__DIR__) . "/App/Libs/Core.php";
+require_once dirname(__DIR__) . "/App/Libs/Request.php";
 //Autload
 spl_autoload_register(function ($clase) {
     require_once dirname(__DIR__) . "/App/Libs/" . $clase . ".php";
